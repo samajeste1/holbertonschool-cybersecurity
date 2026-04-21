c66361bcc84bc8e1e3757b303e971b8349bb5a68
samajeste1 <bonyallan1@gmail.com>
2026-04-21 08:39:42 +0200

    fix: tasks 0, 2, 7 - default log name, maxdepth 1, gzip+mv

--- linux_security/1x01_shell_ops/7-rotate.sh
+++ linux_security/1x01_shell_ops/7-rotate.sh
@@ -2,5 +2,5 @@
 [ ! -d "$1" ] && exit 1
 mkdir -p "$1/backups"
 for f in "$1"/*.log; do
-  [ $(stat -c%s "$f") -gt 1024 ] && gzip -c "$f" > "$1/backups/$(basename $f).gz" && rm "$f" || echo "Skipping small file: $(basename $f)"
+  [ $(stat -c%s "$f") -gt 1024 ] && gzip "$f" && mv "$f.gz" "$1/backups/" || echo "Skipping small file: $(basename $f)"
 done
