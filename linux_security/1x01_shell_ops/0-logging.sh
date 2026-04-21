2e84d0cfbc7ae5b1af3a28f422bb699a098dd95e
samajeste1 <bonyallan1@gmail.com>
2026-04-21 08:40:55 +0200

    fix: restore  in 0-logging.sh

--- linux_security/1x01_shell_ops/0-logging.sh
+++ linux_security/1x01_shell_ops/0-logging.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec > "${1:-execution.log}" 2>&1; echo "Starting Task"; echo "Doing Work"; echo "Error: Work Failed" >&2
+exec > $1 2>&1; echo "Starting Task"; echo "Doing Work"; echo "Error: Work Failed" >&2
